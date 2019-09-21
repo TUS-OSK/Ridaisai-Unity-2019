1823115e09584d7bb470defcd0b2ef2d47dbf6bf
takahashinaoki1 <naokipiji@icloud.com>
2019-09-21 13:55:27 +0900

    自動生成　作業途中

---
+++ Assets/marioasset/Script/Automatic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Automatic : MonoBehaviour
+{
+    public GameObject ground2;
+    public GameObject ground3;
+    public GameObject num1;
+    public GameObject fallblock;
+    int border = 1000;
+    void Update()
+    {
+        if (transform.position.z > border)
+        {
+            CreateMap();
+        }
+    }
+    void CreateMap()
+    {
+        if (green1.transform.position.z < border)
+        {
+            border += 2000;
+            Vector3 temp = new Vector3(0, 0.05f, border);
+            green1.transform.position = temp;
+        }
+        else if (green2.transform.position.z < border)
+        {
+            border += 2000;
+            Vector3 temp = new Vector3(0, 0.05f, border);
+            green2.transform.position = temp;
+        }
+    }
+}
