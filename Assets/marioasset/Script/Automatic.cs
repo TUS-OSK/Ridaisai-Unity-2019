97a7f237ae2f67ea555ab78c2482952241903ac7
takahashinaoki1 <naokipiji@icloud.com>
2019-09-21 15:49:21 +0900

    エラー部分をコメントアウトしました

--- Assets/marioasset/Script/Automatic.cs
+++ Assets/marioasset/Script/Automatic.cs
@@ -11,22 +11,22 @@ public class Automatic : MonoBehaviour
     {
         if (transform.position.z > border)
         {
-            CreateMap();
+          //  CreateMap();
         }
     }
-    void CreateMap()
-    {
-        if (green1.transform.position.z < border)
-        {
-            border += 2000;
-            Vector3 temp = new Vector3(0, 0.05f, border);
-            green1.transform.position = temp;
-        }
-        else if (green2.transform.position.z < border)
-        {
-            border += 2000;
-            Vector3 temp = new Vector3(0, 0.05f, border);
-            green2.transform.position = temp;
-        }
-    }
+    //void CreateMap()
+    //{
+    //    if (green1.transform.position.z < border)
+    //    {
+    //        border += 2000;
+    //        Vector3 temp = new Vector3(0, 0.05f, border);
+    //        green1.transform.position = temp;
+    //    }
+    //    else if (green2.transform.position.z < border)
+    //    {
+    //        border += 2000;
+    //        Vector3 temp = new Vector3(0, 0.05f, border);
+    //        green2.transform.position = temp;
+    //    }
+    //}
 }
