c1b2036770404e3eed1249ca179a8bd3028c93bd
Take0821 <ryousuke7165686658@gmail.com>
2019-11-22 19:16:14 +0900

    titleできた

---
+++ Assets/Scripts/Bottoncontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bottoncontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        Debug.Log("クリックされた");
+    }
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
